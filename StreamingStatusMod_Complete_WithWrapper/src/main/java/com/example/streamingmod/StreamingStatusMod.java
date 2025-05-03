c769be266fb22a2e54cc234fd38185894b18bb6e
icchiigameS <64028779+icchiigameS@users.noreply.github.com>
2025-05-04 04:15:31 +0900

    Add files via upload

---
+++ StreamingStatusMod_Complete_WithWrapper/src/main/java/com/example/streamingmod/StreamingStatusMod.java
@@ -0,0 +1,10 @@
+package com.example.streamingmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("streamingstatusmod")
+public class StreamingStatusMod {
+    public StreamingStatusMod() {
+        System.out.println("StreamingStatusMod loaded");
+    }
+}
